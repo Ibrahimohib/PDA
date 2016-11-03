324c2295ac59527c5a35531fbc2f2d3ae23625c2
ibrahimohib <ibrahimohib@gmail.com>
2016-11-03 11:43:46 -0500

    Updated Home Screen Buttons

--- PDA/PDA/ViewController.swift
+++ PDA/PDA/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func goalsButton(_ sender: Any) {
+    }
+    @IBOutlet var goalsText: UILabel!
+    @IBAction func calcButton(_ sender: Any) {
+    }
+    @IBAction func toolsButton(_ sender: Any) {
+    }
+    @IBOutlet var toolsButtonOut: UIButton!
+    @IBOutlet var calcButtonOut: UIButton!
+    @IBOutlet var calcText: UILabel!
+    @IBOutlet var toolsText: UILabel!
+    @IBOutlet var toolsrealOut: UIButton!
     override func viewDidLoad() {
+        self.view.layoutIfNeeded()
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        goalsText.clipsToBounds = true
+        calcText.clipsToBounds = true
+        toolsText.clipsToBounds = true
+        toolsButtonOut.clipsToBounds = true
+        calcButtonOut.clipsToBounds = true
+        toolsrealOut.clipsToBounds = true
+        
+        goalsText.layer.cornerRadius = 0
+        calcText.layer.cornerRadius = 20
+        toolsText.layer.cornerRadius = 20
+        toolsButtonOut.layer.cornerRadius = 30
+        calcButtonOut.layer.cornerRadius = 30
+        toolsrealOut.layer.cornerRadius = 30
+        
     }
 
     override func didReceiveMemoryWarning() {
